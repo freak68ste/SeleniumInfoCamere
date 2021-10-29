4f53c4c79d62423f5493384327e5a7273ca162c0
Stefano Arrigo <s.arrigo@almaviva.it>
2021-10-29 18:51:17 +0200

    first commit ready

---
+++ Test_SeleniumInfocamerfe/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SeleniumForInfoCamere;
+
+namespace Test_SeleniumInfocamerfe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new SeleniumChrome("http://www.google.it");
+
+            driver.StartBrowser();
+
+            driver.CloseBrowser();
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
